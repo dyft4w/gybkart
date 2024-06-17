d51996ee65ac81991f693c5f2602ee4c4108d099
dyft4w <dyft4w@gmail.com>
2024-06-17 16:46:44 +0200

    started working on engine

---
+++ shitkartz/includes.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL_events.h>
+#include <SDL_pixels.h>
+#include <SDL_surface.h>
+#include <SDL_video.h>
+#include <SDL_image.h>
+#include <SDL_vulkan.h>
+
+#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
+
+#include <vulkan/vulkan.h>
+
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <optional>
+#include <deque>
+#include <map>
+#include <set>
+// self explanatory
+constexpr char NAME[] = "Shitkartz";
+constexpr uint32_t SCREEN_HEIGHT=256;
+constexpr uint32_t SCREEN_WIDTH=256;
+constexpr char ASSET_PATH[] = "../../";
--- shitkartz/main.cpp
+++ shitkartz/main.cpp
@@ -1,27 +1,6 @@
-#include <SDL_events.h>
-#include <SDL_pixels.h>
-#include <SDL_surface.h>
-#include <SDL_video.h>
-#include <cstring>
-#include <iostream>
-#include <SDL_image.h>
-#include <SDL2/SDL.h>
-#include <SDL_vulkan.h>
-#include <vulkan/vulkan.h>
-#include <nlohmann/json_fwd.hpp>
-#include <rapidjson/reader.h>
-#include <vector>
-#include <deque>
-#include <map>
-#include <nlohmann/json.hpp>
-#include <fstream>
-// self explanatory
-constexpr char NAME[] = "Shitkartz";
-constexpr int SCREEN_HEIGHT=256;
-constexpr int SCREEN_WIDTH=256;
-constexpr char ASSET_PATH[] = "../../";
+#include "includes.h"
+#include "vk_main.h"
 //constexpr int SCALE=1;
-
 
 // window and its master surface
 SDL_Window* window=NULL;
@@ -30,6 +9,7 @@ SDL_Surface* screenSurface=NULL;
 std::deque<SDL_Surface*> surfaces;
 std::map<std::string, SDL_Surface*> asset_surfaces;
 std::vector<SDL_Event*> events;
+VkEngine *engine;
 
 
 void clearSurfaces(){
@@ -52,33 +32,15 @@ void loadColor(int r, int g, int b){
 	SDL_UpdateWindowSurface(window);
 }
 void loadSurfaces(){
-  std::ifstream f(std::string(ASSET_PATH)+"inventory.json"); 
+  std::string temp=std::string(ASSET_PATH);
+  std::ifstream f(temp+"inventory.json"); 
   nlohmann::json data = nlohmann::json::parse(f);
-  for(int i = 0; i<data["surfaces"].size();i++){
-    std::string temp=std::string(ASSET_PATH);
-    asset_surfaces[data["surfaces"][i]["name"]] = IMG_Load((temp+"inventory.json").c_str());
+  for(auto &i : data["surfaces"]){
+    asset_surfaces[i["name"]] = IMG_Load((temp+std::string(i["path"])).c_str());
   } 
   f.close();
 }
-void initVulkan(){
-  uint32_t extensionCount;
-  const char** extensionNames = 0;
-  SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
-  extensionNames = new const char *[extensionCount];
-  SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensionNames);
-  const VkInstanceCreateInfo instInfo = {
-      VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-      nullptr,                                // pNext
-      0,                                      // flags
-      nullptr,                                // pApplicationInfo
-      0,                                      // enabledLayerCount
-      nullptr,                                // ppEnabledLayerNames
-      extensionCount,                         // enabledExtensionCount
-      extensionNames,                         // ppEnabledExtensionNames
-  };
-  VkInstance vkInst;
-  vkCreateInstance(&instInfo, nullptr, &vkInst);
-}
+
 bool init(){
 	bool success=true;
   loadSurfaces();
@@ -87,6 +49,7 @@ bool init(){
 		success=false;
 	}else{
 		window = SDL_CreateWindow(NAME,SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+    engine = new VkEngine(window);
 		screenSurface = SDL_GetWindowSurface(window);
 		SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format, 0x00,0x00,0x00));
 		loadImage("andiwonder.bmp");
